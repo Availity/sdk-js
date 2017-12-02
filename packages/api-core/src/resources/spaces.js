72de394bd51b9fea08345f42efade8d45ac9888f
Rob McGuinness <rob.mcguinness@availity.com>
2017-12-02 10:51:31 -0500

    feat(api-core): add spaces helper methods

--- packages/api-core/src/resources/spaces.js
+++ packages/api-core/src/resources/spaces.js
@@ -11,4 +11,32 @@ export default class AvSpaces extends AvApi {
     );
     super(http, promise, thisConfig);
   }
+
+  parseSpaceId(query) {
+    const pairs = query.substr(1).split('&');
+
+    let spaceId = '';
+
+    if (Array.isArray(pairs)) {
+      pairs.forEach(item => {
+        const pair = item.split('=');
+        const key = pair[0];
+        if (key === 'spaceId') {
+          spaceId = pair[1] && decodeURIComponent(pair[1]);
+        }
+      });
+    }
+    return spaceId;
+  }
+
+  getSpaceName(spaceId) {
+    return this.query({
+      params: {
+        id: spaceId,
+      },
+      onAfterQuery(data) {
+        return data.name;
+      },
+    });
+  }
 }
