633f94e10fba98cd210259ef161caee0a54afd48
Travis Hutto <Travis.Hutto@availity.com>
2017-12-21 16:43:48 -0500

    Make deleteByTopic use URL Params

--- packages/api-core/src/resources/tests/notification.test.js
+++ packages/api-core/src/resources/tests/notification.test.js
@@ -17,12 +17,11 @@ describe('AvNotification', () => {
 
   test('deleteByTopic() should call query with topic added to params.topicId', () => {
     api = new AvNotification(mockHttp, Promise, {});
-    api.remove = jest.fn();
+    api.request = jest.fn();
 
     const topic = 'test delete topic';
-    const expectedConfig = Object.assign({}, { params: { topicId: topic } });
 
     api.deleteByTopic(topic);
-    expect(api.remove).toHaveBeenLastCalledWith(expectedConfig);
+    expect(api.request).toHaveBeenCalled();
   });
 });
