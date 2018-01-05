31c98c1d111b6bf8cb4457fa8ed3f8cf9cea0b7a
Kasey Powers <kasey.powers@availity.com>
2018-01-05 09:58:18 -0500

    refactor(api-angular): simplify some factories

--- packages/api-angular/src/notification.js
+++ packages/api-angular/src/notification.js
@@ -2,17 +2,10 @@ import angular from 'angular';
 
 import { AvNotification } from '@availity/api-core';
 
-function factory($http, $q, avApiOptions) {
-  class AvNotificationApi extends AvNotification {
-    constructor() {
-      super({
-        http: $http,
-        promise: $q,
-        merge: angular.merge,
-        config: angular.copy(avApiOptions),
-      });
-    }
-  }
-  return new AvNotificationApi();
-}
-export default factory;
+export default ($http, $q, avApiOptions) =>
+  new AvNotification({
+    http: $http,
+    promise: $q,
+    merge: angular.merge,
+    config: angular.copy(avApiOptions),
+  });
--- packages/api-angular/src/spaces.js
+++ packages/api-angular/src/spaces.js
@@ -2,18 +2,10 @@ import angular from 'angular';
 
 import { AvSpaces } from '@availity/api-core';
 
-function factory($http, $q, avApiOptions) {
-  class AvSpacesApi extends AvSpaces {
-    constructor() {
-      super({
-        http: $http,
-        promise: $q,
-        merge: angular.merge,
-        config: angular.copy(avApiOptions),
-      });
-    }
-  }
-  return new AvSpacesApi();
-}
-
-export default factory;
+export default ($http, $q, avApiOptions) =>
+  new AvSpaces({
+    http: $http,
+    promise: $q,
+    merge: angular.merge,
+    config: angular.copy(avApiOptions),
+  });
