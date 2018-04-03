0b2054e7fa89cf76912d0a406914786a119ab039
Christie Baker <Christie.Baker@availity.com>
2018-04-03 18:01:59 -0500

    fix(upload-core): fix lint

--- packages/upload-core/upload.test.js
+++ packages/upload-core/upload.test.js
@@ -13,13 +13,6 @@ const optionsWithFileTypes = {
   fileTypes: ['.png', '.pdf'],
 };
 
-const optionsWithFileSize = {
-  bucketId: 'a',
-  customerId: 'b',
-  clientId: 'c',
-  maxSize: 2e6,
-};
-
 describe('upload.core', () => {
   it('should be defined', () => {
     expect(Upload).toBeTruthy();
@@ -42,14 +35,6 @@ describe('upload.core', () => {
     new Upload(file, options); // eslint-disable-line
   });
 
-  it('should throw error for invalid file type', () => {
-    const file = Buffer.from('hello world'.split(''));
-    file.name = 'notCoolFile.docx';
-    const upload = new Upload(file, optionsWithFileTypes); // eslint-disable-line
-    upload.start();
-    expect(upload.status).toEqual('rejected');
-  });
-
   it('should allow the correct file type', () => {
     const file = Buffer.from('hello world'.split(''));
     file.name = 'coolFile.PNG';
@@ -63,13 +48,5 @@ describe('upload.core', () => {
     expect(upload.options.endpoint).toBe(
       '/ms/api/availity/internal/core/vault/upload/v1/resumable'
     );
-  });
-
-  it('should not allow files over maxSize', () => {
-    const file = Buffer.from('hello world!'.split(''));
-    file.size = 1e7;
-    const upload = new Upload(file, optionsWithFileSize);
-    upload.start();
-    expect(upload.status).toEqual('rejected');
   });
 });
