f2472358bfef0744ec31e7b764ae1586b314af6b
KaseyPowers <kaseyepowers@gmail.com>
2018-08-22 14:50:34 -0400

    feat(api-core): helper function for settings api (#64)

--- packages/api-core/src/resources/settings.js
+++ packages/api-core/src/resources/settings.js
@@ -1,11 +1,13 @@
 import AvApi from '../api';
 
 export default class AvSettings extends AvApi {
-  constructor({ http, promise, merge, config }) {
+  constructor({ http, promise, merge, avUsers, config }) {
     const options = Object.assign(
       {
         path: 'api/utils',
         name: 'settings',
+        sessionBust: false,
+        pageBust: true,
       },
       config
     );
@@ -14,6 +16,42 @@ export default class AvSettings extends AvApi {
       promise,
       merge,
       config: options,
+    });
+    this.avUsers = avUsers;
+  }
+
+  getApplication(applicationId, config) {
+    if (!applicationId) {
+      throw new Error('must define applicationId');
+    }
+    return this.avUsers.me().then(user => {
+      const queryConfig = this.addParams(
+        { applicationId, userId: user.id },
+        config
+      );
+      return this.query(queryConfig);
+    });
+  }
+
+  setApplication(applicaitonId, data, config) {
+    if (
+      typeof applicaitonId !== 'string' &&
+      typeof applicaitonId !== 'number'
+    ) {
+      config = data;
+      data = applicaitonId;
+      applicaitonId = '';
+    }
+
+    if (!applicaitonId && (!data.scope || !data.scope.applicationId)) {
+      throw new Error('must set applicationId in settings call');
+    }
+
+    return this.avUsers.me().then(user => {
+      data.scope = data.scope || {};
+      data.scope.applicationId = applicaitonId;
+      data.scope.userId = user.id;
+      return this.update(data, config);
     });
   }
 }
