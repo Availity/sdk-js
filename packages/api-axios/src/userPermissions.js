b6f33ba8a62176115d583dd22fc4795e196f47f3
John Sarman <John.Sarman@availity.com>
2018-08-22 14:39:09 -0400

    fix(api-core): Allow array for permissions
    axi-user-permissions supports queryParms of arrays as so permissionId=1234&permissionId=2345.  Add paramSerializer to override default.

--- packages/api-axios/src/userPermissions.js
+++ packages/api-axios/src/userPermissions.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import merge from 'deep-assign';
 import { AvUserPermissions } from '@availity/api-core';
+import qs from 'qs';
 
 export default new AvUserPermissions({
   http: axios,
   promise: Promise,
   merge,
+  config: {
+    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
+  }
 });
