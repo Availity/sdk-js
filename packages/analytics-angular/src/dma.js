80f323d644e78ba4328254ec3129212c0733bcc0
Kyle Gray <Kyle.Gray@availity.com>
2019-06-21 15:15:08 -0400

    chore: removed analytics-angular package

--- packages/analytics-angular/src/dma.js
+++
@@ -1,7 +0,0 @@
-import { AvDmaAnalytics } from '@availity/analytics-core';
-
-function AvDmaAnalyticsFactory(avLogMessagesApi) {
-  return new AvDmaAnalytics(avLogMessagesApi);
-}
-
-export default AvDmaAnalyticsFactory;
--- packages/analytics-angular/src/splunk.js
+++
@@ -1,7 +0,0 @@
-import { AvSplunkAnalytics } from '@availity/analytics-core';
-
-function AvSplunkAnalyticsFactory(avLogMessagesApi) {
-  return new AvSplunkAnalytics(avLogMessagesApi);
-}
-
-export default AvSplunkAnalyticsFactory;
