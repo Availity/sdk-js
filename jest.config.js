c9cbf0db2d73d7744a3b8476b0c23cbbeb327a81
Kyle Gray <kgray1497@gmail.com>
2019-09-27 15:20:05 -0400

    ci: updated jest globals deps

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,14 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>.*(node_modules)(?!.*@availity.*).*$',
     '<rootDir>/plopfile.js',
+    '/packages/docs/',
   ],
-  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    '/dist/',
+    '/packages/docs/',
+  ],
   testEnvironment: 'jest-environment-jsdom-global',
   testURL: 'http://localhost:8080',
   globals: {
