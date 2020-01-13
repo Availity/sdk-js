0792f85b8be73e2eb3bef465c181c7810746e981
Jordan Young <jordan.young@availity.com>
2020-01-13 15:50:58 -0500

    fix(api-core): fixed error trying to call toString on undefined

---
+++ packages/api-core/src/tests/flattenObject.test.js
@@ -0,0 +1,32 @@
+import flattenObject from '../flattenObject';
+
+describe('AvApi', () => {
+  test('should flatten', () => {
+    const obj = {
+      int: 123,
+      obj: {
+        string: 'string',
+      },
+    };
+
+    const flatObj = flattenObject(obj);
+    expect(flatObj.int).toBe('123');
+    expect(flatObj['obj.string']).toBe('string');
+  });
+
+  test('handles undefined', () => {
+    const obj = {
+      int: 123,
+      string: undefined,
+      obj: {
+        int: 123,
+        string: 'string',
+      },
+    };
+
+    const flatObj = flattenObject(obj);
+    expect(flatObj.int).toBe('123');
+    expect(flatObj['obj.string']).toBe('string');
+    expect(flatObj.string).toBeUndefined();
+  });
+});
