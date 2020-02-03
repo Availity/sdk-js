bc5ffae4c9f5277484174b200670e4d6f0ee5600
Kyle Gray <kyle.gray@availity.com>
2020-02-03 18:38:12 -0500

    chore(relay-id): exported type defs

--- packages/relay-id/index.d.ts
+++ packages/relay-id/index.d.ts
@@ -7,3 +7,5 @@ declare function base64(decodedString: string): string;
 declare function unbase64(encodedString: string): string;
 declare function toGlobalId(type: string, id: string): string;
 declare function fromGlobalId(globalId: string): ResolvedGlobalId;
+
+export { base64, unbase64, toGlobalId, fromGlobalId };
