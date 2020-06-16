36e69bbd9dc68eb3c401489b48e8084821a003e4
Christopher Havekost <Christopher.Havekost@availity.com>
2020-06-16 11:00:10 -0400

    fix: check correct lockfile for artifactory refs, include as pre-push script

--- scripts/artifactory-check.sh
+++ scripts/artifactory-check.sh
@@ -12,10 +12,10 @@
 # error out if something fails
 set -e
 
-if grep -R --exclude='*.sh' 'artifactory.availity' --include='package-lock.json' ./
+if grep -R --exclude='*.sh' 'artifactory.availity' --include='yarn.lock' ./
 then
   printf "\nOne of your packages contains a dependency from registry: artifactory.availity.com.\n"
-  printf "Please correct this by running 'npm run nuke' and then 'npm install'.\n\n"
+  printf "Please correct this by running 'yarn nuke' and then 'yarn'.\n\n"
   exit 1
 else
   echo "Artifactory Check Passed"
