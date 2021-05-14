2aebb4b5ac512b8be1818a97914b84e6b734b1da
Christopher Havekost <Christopher.Havekost@availity.com>
2021-05-14 12:26:11 -0400

    fix(resolve-url): fixes incorrect regex results failing tests, assign default param to baseIRI

--- packages/resolve-url/src/is-absolute-url.js
+++ packages/resolve-url/src/is-absolute-url.js
@@ -1,10 +1,11 @@
 // Borrowed from https://github.com/sindresorhus/is-absolute-url to make IE11 compatible
-const isAbsoluteUrl = url => {
+const isAbsoluteUrl = (url) => {
   if (typeof url !== 'string') {
     throw new TypeError(`Expected a \`string\`, got \`${typeof url}\``);
   }
 
-  return /^[a-z][\d+.a-z-]*:/.test(url);
+  // eslint-disable-next-line unicorn/better-regex
+  return /^[a-z][a-z\d+.-]*:/.test(url);
 };
 
 export default isAbsoluteUrl;
