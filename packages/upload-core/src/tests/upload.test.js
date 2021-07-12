4b15d357f7c9b869e366fd5faeeea0cf67969411
Daniel Apatiga <daniel.apatiga@availity.com>
2021-07-12 16:53:51 -0400

    fix(upload-core): continuation of last commit

--- packages/upload-core/src/tests/upload.test.js
+++ packages/upload-core/src/tests/upload.test.js
@@ -285,6 +285,12 @@ describe('upload-core', () => {
           upload.start();
         }));
 
+      it('should pickup upload object on each array of functions in onPreStart', () => {
+          const file = Buffer.from('hello world!');
+          file.name = 'a';
+          const upload = new Upload(file, optionsWithOnPreStartFail);
+          upload.start();
+        });
       it('should start upload if all the functions in onPreStart returns true', () =>
         new Promise((resolve) => {
           nock('https://dev.local')
