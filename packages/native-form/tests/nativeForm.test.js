23a1bd98cc71334454d137be822a5a5a23a32447
Christopher Havekost <Christopher.Havekost@availity.com>
2021-08-30 12:15:10 -0400

    feat(native-form): call avWebQLApi to obtain sso type
    BREAKING CHANGE: native-form now exports an async function, no longer defaults type
    MIGRATION:
     Before: nativeForm(...args);
     After: await nativeForm(...args);

--- packages/native-form/tests/nativeForm.test.js
+++ packages/native-form/tests/nativeForm.test.js
@@ -1,5 +1,8 @@
+import { avWebQLApi } from '@availity/api-axios';
 import nativeForm from '../src';
 import flattenObject from '../src/flattenObject';
+
+jest.mock('@availity/api-axios');
 
 describe('nativeForm', () => {
   const complexObject = {
@@ -40,6 +43,7 @@ describe('nativeForm', () => {
       });
     });
   });
+
   describe('default export', () => {
     beforeEach(() => {
       window.HTMLFormElement.prototype.submit = jest.fn();
@@ -49,103 +53,111 @@ describe('nativeForm', () => {
       const form = document.querySelector('form');
       if (form) form.remove();
     });
-    test('spaceId is required', () => {
-      expect(() => nativeForm()).toThrow(
+
+    test('spaceId is required', async () => {
+      await expect(() => nativeForm()).rejects.toThrow(
         'spaceId is required and was not provided'
       );
     });
 
-    test('create a form', () => {
-      nativeForm('spaceId');
+    test('create a form', async () => {
+      await nativeForm('spaceId', {}, {}, 'saml');
       expect(document.querySelector('form')).not.toBeNull();
     });
 
     describe('the form', () => {
-      test('should be added to the body (to be able to be submitted)', () => {
-        nativeForm('spaceId');
+      test('should be added to the body (to be able to be submitted)', async () => {
+        await nativeForm('spaceId', {}, {}, 'saml');
         expect(document.body.querySelector('form')).not.toBeNull();
       });
 
-      test('action should have the space id in the URL by default', () => {
-        nativeForm('spaceId123');
+      test('action should have the space id in the URL by default', async () => {
+        await nativeForm('spaceId123', {}, {}, 'saml');
         expect(document.querySelector('form').getAttribute('action')).toBe(
           '/ms/api/availity/internal/spc/magneto/sso/v1/saml/spaceId123'
         );
       });
 
-      test('action should be overridable', () => {
-        nativeForm('spaceId123', {}, { action: '/my/url/here' });
+      test('action should be overridable', async () => {
+        await nativeForm('spaceId123', {}, { action: '/my/url/here' }, 'saml');
         expect(document.querySelector('form').getAttribute('action')).toBe(
           '/my/url/here'
         );
       });
 
-      test('action magneto integration type should be overridable', () => {
-        nativeForm('spaceId123', {}, {}, 'openid');
+      test('action magneto integration type should be overridable', async () => {
+        await nativeForm('spaceId123', {}, {}, 'openid');
         expect(document.querySelector('form').getAttribute('action')).toBe(
           '/ms/api/availity/internal/spc/magneto/sso/v1/openid/spaceId123'
         );
       });
 
-      test('method should be post by default', () => {
-        nativeForm('spaceId');
+      test('method should be post by default', async () => {
+        await nativeForm('spaceId', {}, {}, 'saml');
         expect(document.querySelector('form').getAttribute('method')).toBe(
           'post'
         );
       });
 
-      test('method should be overridable', () => {
-        nativeForm('spaceId123', {}, { method: 'get' });
+      test('method should be overridable', async () => {
+        await nativeForm('spaceId123', {}, { method: 'get' }, 'saml');
         expect(document.querySelector('form').getAttribute('method')).toBe(
           'get'
         );
       });
 
-      test('target should be _blank by default', () => {
-        nativeForm('spaceId');
+      test('target should be _blank by default', async () => {
+        await nativeForm('spaceId', {}, {}, 'saml');
         expect(document.querySelector('form').getAttribute('target')).toBe(
           '_blank'
         );
       });
 
-      test('target should be overridable', () => {
-        nativeForm('spaceId123', {}, { target: '_top' });
+      test('target should be overridable', async () => {
+        await nativeForm('spaceId123', {}, { target: '_top' }, 'saml');
         expect(document.querySelector('form').getAttribute('target')).toBe(
           '_top'
         );
       });
 
-      test('addtional attributes can be defined', () => {
-        nativeForm('spaceId123', {}, { id: 'myForm', class: 'my-form' });
+      test('addtional attributes can be defined', async () => {
+        await nativeForm(
+          'spaceId123',
+          {},
+          { id: 'myForm', class: 'my-form' },
+          'saml'
+        );
+
         expect(document.querySelector('form').getAttribute('id')).toBe(
           'myForm'
         );
+
         expect(document.querySelector('form').getAttribute('class')).toBe(
           'my-form'
         );
       });
 
-      test('should have no field if no data was provided', () => {
-        nativeForm('spaceId123');
+      test('should have no field if no data was provided', async () => {
+        await nativeForm('spaceId123', {}, {}, 'saml');
         expect(document.querySelector('input')).toBeNull();
       });
 
-      test('should have fields if was provided', () => {
-        nativeForm('spaceId123', { a: 'b' });
+      test('should have fields if was provided', async () => {
+        await nativeForm('spaceId123', { a: 'b' }, {}, 'saml');
         expect(document.querySelector('input')).not.toBeNull();
       });
 
       describe('the fields', () => {
-        test('should have a field for each piece of data (no more, no less)', () => {
-          nativeForm('spaceId123', complexObject);
+        test('should have a field for each piece of data (no more, no less)', async () => {
+          await nativeForm('spaceId123', complexObject, {}, 'saml');
           expect(document.querySelectorAll('input').length).toBe(
             Object.keys(flattenObject(complexObject)).length
           );
         });
 
-        test('the names will match the flat object, but have the array index value removed (the way the back-end likes it)', () => {
+        test('the names will match the flat object, but have the array index value removed (the way the back-end likes it)', async () => {
           // Note: this causes multiple fields to have the same name, so we have to account for that
-          nativeForm('spaceId123', complexObject);
+          await nativeForm('spaceId123', complexObject, {}, 'saml');
           const flat = flattenObject(complexObject);
           const count = {};
           for (const key of Object.keys(flat)) {
@@ -158,9 +170,9 @@ describe('nativeForm', () => {
           }
         });
 
-        test('data should match the values within the object', () => {
+        test('data should match the values within the object', async () => {
           // Note: this causes multiple fields to have the same name, so we have to account for that
-          nativeForm('spaceId123', complexObject);
+          await nativeForm('spaceId123', complexObject, {}, 'saml');
           const flat = flattenObject(complexObject);
           const count = {};
           for (const key of Object.keys(flat)) {
@@ -176,9 +188,87 @@ describe('nativeForm', () => {
       });
     });
 
-    test('submit the form', () => {
-      nativeForm('spaceId');
+    test('submit the form', async () => {
+      await nativeForm('spaceId', {}, {}, 'saml');
       expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalled();
+    });
+
+    describe('fetch sso type behavior', () => {
+      beforeEach(() => {
+        jest.clearAllMocks();
+      });
+
+      test('skips call to webQL endpoint when type param is saml', async () => {
+        avWebQLApi.create.mockResolvedValue({
+          data: {
+            data: {
+              configurationFindOne: {
+                type: null,
+              },
+            },
+          },
+        });
+
+        await nativeForm('spaceId', {}, {}, 'saml');
+        expect(avWebQLApi.create).toHaveBeenCalledTimes(0);
+        expect(document.querySelector('form').getAttribute('action')).toBe(
+          '/ms/api/availity/internal/spc/magneto/sso/v1/saml/spaceId'
+        );
+      });
+
+      test('skips call to webQL endpoint when type param is openid', async () => {
+        avWebQLApi.create.mockResolvedValue({
+          data: {
+            data: {
+              configurationFindOne: {
+                type: null,
+              },
+            },
+          },
+        });
+
+        await nativeForm('spaceId', {}, {}, 'openid');
+        expect(avWebQLApi.create).toHaveBeenCalledTimes(0);
+        expect(document.querySelector('form').getAttribute('action')).toBe(
+          '/ms/api/availity/internal/spc/magneto/sso/v1/openid/spaceId'
+        );
+      });
+
+      test('calls webQL endpoint when type param is omitted', async () => {
+        avWebQLApi.create.mockResolvedValue({
+          data: {
+            data: {
+              configurationFindOne: {
+                type: 'SAML',
+              },
+            },
+          },
+        });
+
+        await nativeForm('spaceId', {}, {});
+        expect(avWebQLApi.create).toHaveBeenCalledTimes(1);
+        expect(document.querySelector('form').getAttribute('action')).toBe(
+          '/ms/api/availity/internal/spc/magneto/sso/v1/saml/spaceId'
+        );
+      });
+
+      test('calls webQL endpoint when type param is neither saml nor openid', async () => {
+        avWebQLApi.create.mockResolvedValue({
+          data: {
+            data: {
+              configurationFindOne: {
+                type: 'OPENID',
+              },
+            },
+          },
+        });
+
+        await nativeForm('spaceId', {}, {}, 'application');
+        expect(avWebQLApi.create).toHaveBeenCalledTimes(1);
+        expect(document.querySelector('form').getAttribute('action')).toBe(
+          '/ms/api/availity/internal/spc/magneto/sso/v1/openid/spaceId'
+        );
+      });
     });
   });
 });
