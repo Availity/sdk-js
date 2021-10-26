552f15d9092507ccd9c050e908cb5f5bf662d8e1
Greg Martin <greg.martin@availity.com>
2021-10-26 14:50:09 -0400

    refactor(dockyard): compileRequiredFields support

--- packages/dockyard/src/dockyard.js
+++ packages/dockyard/src/dockyard.js
@@ -2,6 +2,7 @@ const addDelimiter = (a, b) => (a ? `${a}.${b}` : b);
 
 const transformRules = (schemaFieldDocs, options) => {
   const fieldDocs = [];
+  let isRequired = false;
   if (schemaFieldDocs.notOneOf?.length > 0) {
     fieldDocs.push(`not one of: ${schemaFieldDocs.notOneOf.join(', ')}`);
   }
@@ -10,7 +11,9 @@ const transformRules = (schemaFieldDocs, options) => {
   }
   if (schemaFieldDocs.tests?.length > 0) {
     for (const test of schemaFieldDocs.tests) {
-      if (test.params) {
+      if (options.compileRequiredFields && test.name === 'required') {
+        isRequired = true;
+      } else if (test.params) {
         const params = [];
         for (const param in test.params) {
           if (Object.prototype.hasOwnProperty.call(test.params, param)) {
@@ -31,14 +34,26 @@ const transformRules = (schemaFieldDocs, options) => {
   }
 
   const friendlyFieldDocs = fieldDocs.length > 0 ? `Rules: ${fieldDocs.join(', ')}.` : '';
-  return friendlyFieldDocs;
+  return options.compileRequiredFields ? { fieldDocs: friendlyFieldDocs, isRequired } : friendlyFieldDocs;
 };
+
+const getRequiredFields = (rules) =>
+  Object.entries(rules).reduce((obj, [key, value]) => {
+    Object.assign(obj, { [key]: value.fieldDocs });
+    if (value.isRequired) {
+      (obj.requiredFields = obj.requiredFields || []).push(key);
+    }
+
+    return obj;
+  }, {});
 
 const buildRules = (fields, head = '', options) =>
   Object.entries(fields).reduce((obj, [key, value]) => {
     const path = addDelimiter(head, key);
 
-    Object.assign(obj, { [path]: transformRules(value, options) });
+    const rules = transformRules(value, options);
+
+    Object.assign(obj, { [path]: rules });
 
     if (value?.fields) {
       Object.assign(obj, buildRules(value.fields, key, options));
@@ -58,10 +73,16 @@ const buildRules = (fields, head = '', options) =>
  * @param {*} options.includeOneOf
  * @returns
  */
-const getRules = (validation, { compileRequiredFields = false, includeOneOf = false }) => {
-  const { fields } = validation.describe();
+const getRules = (validation, { compileRequiredFields = false, includeOneOf = false } = {}) => {
+  const description = validation.describe();
 
-  return buildRules(fields, '', { compileRequiredFields, includeOneOf });
+  const rules = buildRules(description.fields, '', { compileRequiredFields, includeOneOf });
+
+  if (compileRequiredFields) {
+    return getRequiredFields(rules);
+  }
+
+  return rules;
 };
 
 module.exports = { getRules, buildRules };
