91ac991b2a812eb157446925d80721e047ad5b87
Jordan Young <jordan.young@availity.com>
2022-04-18 17:02:10 -0400

    build: add path mapping

--- packages/analytics-core/jest.config.js
+++ packages/analytics-core/jest.config.js
@@ -1,3 +1,7 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   displayName: 'analytics-core',
   preset: '../../jest.preset.js',
@@ -7,4 +11,5 @@ module.exports = {
     },
   },
   coverageDirectory: '../../coverage/analytics-core',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/../../' }),
 };
