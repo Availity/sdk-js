8c6c9c687aa22d48413211707ff3c0ac2dbf0368
Greg Martin <greg.martin@availity.com>
2026-04-09 14:59:00 -0400

    fix(api-axios): filter undefined and null fields

--- packages/api-axios/src/resources/dma.js
+++ packages/api-axios/src/resources/dma.js
@@ -20,6 +20,7 @@ export default class AvLogMessagesApiV2 extends AvMicroserviceApi {
     flattened.X_XSRF_TOKEN = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*=\s*([^;]*).*$)|^.*$/, '$1');
 
     const fields = Object.keys(flattened)
+      .filter((key) => flattened[key] != null)
       .map((key) => {
         const name = key.replaceAll(/\[\d+]/g, '[]');
         const value = flattened[key];
